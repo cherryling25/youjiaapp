1b5c9a9eac7a66d7543eff1867d1850d4572c0bd
Cherry <cherryling25@outlook.com>
2019-07-24 23:21:32 +0800

    home 页面

--- youjia_app/pages/home/home.vue
+++ youjia_app/pages/home/home.vue
@@ -1,5 +1,34 @@
 <template>
 	<view>
+		<!--轮播-->
+		<view class="uni-padding-wrap">
+            <view class="page-section swiper">
+                <view class="page-section-spacing">
+                    <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+                        <swiper-item>
+                            <view class="swiper-item">
+								<image src="../../static/switch.jpeg" mode=""></image>
+							</view>
+                        </swiper-item>
+						
+                        <swiper-item>
+                            <view class="swiper-item">
+								<image src="../../static/switch.jpeg" mode=""></image>
+							</view>
+                        </swiper-item>
+						
+                        <swiper-item>
+                            <view class="swiper-item">
+								<image src="../../static/switch.jpeg" mode=""></image>
+							</view>
+                        </swiper-item>
+						
+                    </swiper>
+                </view>
+            </view>
+        </view>
+		
+		<!--table-->
 		
 	</view>
 </template>
@@ -8,7 +37,10 @@
 	export default {
 		data() {
 			return {
-				
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
 			}
 		},
 		methods: {
